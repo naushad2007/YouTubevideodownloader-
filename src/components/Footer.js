3269757014253d1a04a87a602bf68d5dd9348244
Naushad Alam <64242470+naushad2007@users.noreply.github.com>
2021-11-21 22:16:57 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
     return (
         <div style={{width:"100%",backgroundColor:"black",color:"#fff",height:"10vh",display:"flex",justifyContent:"center"}}>
-            <p style={{textAlign:"center"}}>  you-tubevideodownloader-786 </p>
+            <p style={{textAlign:"center"}}>  copyright 2021 All Right Reserved || you-tubevideodownloader-786 </p>
         </div>
     )
 }
