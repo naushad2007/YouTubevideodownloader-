82a385d5a9ac14be2724f5fea005e9259fdf91f1
Naushad Alam <64242470+naushad2007@users.noreply.github.com>
2021-11-22 07:13:29 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Footer from "./components/Footer"
+import Footer from "./components/Footer";
 import {
   BrowserRouter as Router,
 } from "react-router-dom";
